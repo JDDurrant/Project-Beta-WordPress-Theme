2a1058f9a3b61f6ab9b5e263e9beb0a63ef01568
Jack Durrant <jack@jackdurrant.com>
2013-01-06 00:54:57 +0000

    minor changes
    -Removed broken IE JavaScript code
    -Removed unneccessary files

--- header.php
+++ header.php
@@ -7,10 +7,6 @@
 		<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url'); ?>" title="<?php printf( __( '%s Latest Posts', 'your-theme' ), wp_specialchars( get_bloginfo('name'), 1 ) ); ?>" />
 		<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url'); ?>" title="<?php printf( __( '%s Latest Comments', 'your-theme' ), wp_specialchars( get_bloginfo('name'), 1 ) ); ?>" />
 		<meta name="description" content="<?php bloginfo('description'); ?>" />
-		<!-- [if IE]>
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
-		<script src="<?php bloginfo('template_directory'); ?>/iescript.js" type="text/javascript"></script>
-		<![endif]-->
 		<?php wp_head(); ?>
 	</head>
 	<body>
